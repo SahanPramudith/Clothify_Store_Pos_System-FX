7945a49797fdcf2d2750d5f33172b5480c75bb77
sahanpramudith <pramuditsahan466@gmail.com>
2024-10-14 18:52:22 +0530

    date and time set

--- src/main/java/controller/Dashboard.java
+++ src/main/java/controller/Dashboard.java
@@ -1,15 +1,48 @@
 package controller;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
-public class Dashboard {
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+
+public class Dashboard implements Initializable {
+
+
 
     @FXML
-    private Rectangle lbldate;
+    private Label lbldate;
 
     @FXML
     private Label lbltieme;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        displayTime();
+    }
+
+    private void displayTime(){
+        Timeline date=new Timeline(new KeyFrame(Duration.ZERO,
+                e->lbldate.setText(LocalDate.now().
+                        format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))),
+                new KeyFrame((Duration.seconds(1))));
+        date.setCycleCount(Animation.INDEFINITE);
+        date.play();
+        Timeline time=new Timeline(new KeyFrame(Duration.ZERO,
+                e-> lbltieme.setText(LocalDateTime.now().
+                        format(DateTimeFormatter.ofPattern("HH:mm:ss")))),
+                new KeyFrame(Duration.seconds(1)));
+
+        time.setCycleCount(Animation.INDEFINITE);
+        time.play();
+    }
 }
