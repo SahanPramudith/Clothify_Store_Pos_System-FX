07777d90d1562785f814b29370c8e13a39ba0683
sahanpramudith <pramuditsahan466@gmail.com>
2024-10-14 18:39:06 +0530

    create dashboard form

---
+++ src/main/java/controller/Dashboard.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Rectangle;
+
+public class Dashboard {
+
+    @FXML
+    private Rectangle lbldate;
+
+    @FXML
+    private Label lbltieme;
+
+}
--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -3,23 +3,40 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.EventObject;
 
 public class LoginFormController {
 
+    private Parent parent;
+    private Stage stage;
+    private EventObject actionEvent;
+    private Scene scene;
     @FXML
-    void btnLoginDashBoardOnAction(ActionEvent event) {
-        Stage stage = new Stage();
-        try {
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(""))));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        stage.setResizable(false);
+    void btnLoginDashBoardOnAction(ActionEvent event) throws IOException {
 
+
+        parent= FXMLLoader.load(getClass().getResource("../view/dashboard.fxml"));
+        stage= (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.show();
+
+
+//        Stage stage = new Stage();
+//        try {
+//            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/dashboard.fxml"))));
+//            stage.show();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        stage.setResizable(false);
+//
     }
 
 }
