0a3015c97d416524111d66796946cbe94c25f06f
sahanpramudith <pramuditsahan466@gmail.com>
2024-10-15 08:40:01 +0530

    crate item code

---
+++ src/main/java/controller/AddItemFormController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class AddItemFormController {
+    public void btnAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnClearOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBackOnAction(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/controller/Dashboard.java
+++ src/main/java/controller/Dashboard.java
@@ -3,19 +3,32 @@ package controller;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.EventObject;
 import java.util.ResourceBundle;
 
+
 public class Dashboard implements Initializable {
+    private Parent parent;
+    private Stage stage;
+    private EventObject actionEvent;
+    private Scene scene;
 
 
 
@@ -44,5 +57,25 @@ public class Dashboard implements Initializable {
 
         time.setCycleCount(Animation.INDEFINITE);
         time.play();
+    }
+
+    public void btnOdetOnAction(ActionEvent actionEvent) throws IOException {
+        parent= FXMLLoader.load(getClass().getResource("../view/add_item.fxml"));
+        stage= (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void btnEmployeeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnItenOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnOrderDetailsnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSupplierOnAction(ActionEvent actionEvent) {
     }
 }
